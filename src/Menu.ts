256c46f609f9502e3e4c1c8da985604bc1742eea
Matheus <matheushnrk@outlook.com>
2025-09-09 11:30:16 -0300

    Primeiro commit - menu do banco dos bancos

---
+++ src/Menu.ts
@@ -0,0 +1,72 @@
+
+import * as readlineSync from "readline-sync";
+import { Colors } from "./util/Colors";
+
+export class Menu {
+    static main(): void {
+        let opcao: number;
+
+        do {
+            console.clear();
+            console.log(Colors.FgGreen + "***************************************************************************************************" + Colors.Reset);
+            console.log(Colors.FgCyan + "                             BANCO DOS BANCOS - O BANCO QUE TEM DINHEIRO                           " + Colors.Reset);
+            console.log(Colors.FgGreen + "***************************************************************************************************" + Colors.Reset);
+            console.log("");
+            console.log("                             1 - Criar conta");
+            console.log("                             2 - Listar todas as contas");
+            console.log("                             3 - Buscar conta por número");
+            console.log("                             4 - Atualizando dados da conta");
+            console.log("                             5 - Apagar Conta");
+            console.log("                             6 - Sacar");
+            console.log("                             7 - Depositar");
+            console.log("                             8 - Transferir valores entre contas");
+            console.log("                             9 - Sair");
+            console.log("");
+
+            opcao = readlineSync.questionInt("Entre com a opção desejada: ");
+
+            switch (opcao) {
+                case 1:
+                    console.log("\nCriar Conta\n");
+                    break;
+                case 2:
+                    console.log("\nListar todas as contas\n");
+                    break;
+                case 3:
+                    console.log("\nConsultar dados da conta - por número\n");
+                    break;
+                case 4:
+                    console.log("\nAtualizar dados da conta\n");
+
+                    break;
+                case 5:
+                    console.log("\nApagar uma conta\n");
+
+                    break;
+                case 6:
+                    console.log("\nSaque\n");
+
+                    break;
+                case 7:
+                    console.log("\nDepósito\n");
+
+                    break;
+                case 8:
+                    console.log("\nTransferir valores entre contas\n");
+
+                    break;
+                case 9:
+                    console.log("\nBanco dos Bancos - aqui tem muito dinheiro!");
+                    break;
+                default:
+                    console.log("\nOpção inválida! Tente novamente.");
+            }
+
+            if (opcao !== 9) readlineSync.question("\nPressione Enter para voltar ao menu...");
+        } while (opcao !== 9);
+    }
+}
+
+if (require.main === module) {
+    Menu.main();
+}
---
+++ src/util/Colors.ts
@@ -0,0 +1,11 @@
+// src/util/Colors.ts
+export class Colors {
+    static Reset = "\x1b[0m";
+    static FgRed = "\x1b[31m";
+    static FgGreen = "\x1b[32m";
+    static FgYellow = "\x1b[33m";
+    static FgBlue = "\x1b[34m";
+    static FgMagenta = "\x1b[35m";
+    static FgCyan = "\x1b[36m";
+    static FgWhite = "\x1b[37m";
+}
